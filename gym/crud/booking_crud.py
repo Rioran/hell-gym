34441d459ca40348a2d99d73ba422854f585783f
DaniilAmoshiy1 <daniil13022007god@gmail.com>
2024-08-10 14:36:10 +0400

    fix: move function add_new_trainer from booking_crud.py to trainer_crud.py

--- gym/crud/booking_crud.py
+++ gym/crud/booking_crud.py
@@ -1,11 +1,1 @@
-from gym.datamodel import Trainer
-from gym.db_utilities.session import GymSession
 
-
-def add_new_trainer(trainer_name, trainer_bio='Not filled'):
-    with GymSession.get_session() as session:
-        name = Trainer(name=trainer_name)
-        bio = Trainer(bio=trainer_bio)
-        all_items = [name, bio]
-        session.add_all(all_items)
-        session.commit()
