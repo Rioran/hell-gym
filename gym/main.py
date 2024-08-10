aa40bdef1c25ca5f54f7bb57633ecb2828a0e287
lailache <laila.choudkhuri@quantori.academy>
2024-08-10 14:49:20 +0400

    feauture: add new command line

--- gym/main.py
+++ gym/main.py
@@ -16,6 +16,17 @@ def main():
         trainers = get_recent_trainers()
         print_entries(trainers)
 
+    if arguments.ask_for_client:
+        trainer_name = arguments.ask_for_client
+        trainer = get_trainer_by_name(trainer_name)
+        if trainer:
+            client = get_client()  # You need to implement this function
+            if client:
+                add_client_to_trainer(trainer, client)
+            else:
+                print("No client available to add to the trainer.")
+        else:
+            print(f"No trainer found with the name {trainer_name}")
 
 if __name__ == '__main__':
     main()
