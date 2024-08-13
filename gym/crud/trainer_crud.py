a0142e152cc794f591007efcf90aac63b7988952
DemiAnderson <demiusg@gmail.com>
2024-08-13 17:01:01 +0300

    Feature: add check trainer availability and check for first free client

--- gym/crud/trainer_crud.py
+++ gym/crud/trainer_crud.py
@@ -1,4 +1,6 @@
-from gym.datamodel import Trainer
+from sqlalchemy import select
+
+from gym.datamodel import Booking, BookingUpdate, Trainer
 from gym.db_utilities import GymSession
 from gym.config import PRINT_ITEMS_PER_PAGE
 
@@ -10,3 +12,27 @@ def get_recent_trainers(limit=PRINT_ITEMS_PER_PAGE, offset=0):
             query = query.limit(limit).offset(offset)
         data = query.all()
     return data
+
+
+def get_trainer_by_name(trainer_name):
+    with GymSession.get_session() as session:
+        trainer = session.query(Trainer).filter(Trainer.name == trainer_name).first()
+        return trainer
+
+
+def is_trainer_available(trainer_name):
+    trainer = get_trainer_by_name(trainer_name)
+    if trainer:
+        return True
+    return False
+
+def get_unoccupied_client():
+    with GymSession.get_session() as session:
+        unoccupied_client = (
+            session.query(Booking)
+            .outerjoin(BookingUpdate)
+            .filter(BookingUpdate.id == None)
+            .order_by(Booking.id)
+            .first()
+        )
+        return unoccupied_client
